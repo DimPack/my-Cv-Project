28c67def789ca0f942eed2881413bc43ee809b89
Dima <my_email@gmail.com>
2025-06-23 22:16:01 +0300

    created a contact block

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,11 +1,17 @@
 import phoneIcon from "../../assets/phone.svg";
 import letterIcon from "../../assets/envelope.svg";
 import locationIcon from "../../assets/location.svg";
-import './Contacts.sass';
+import planetIcon from "../../assets/planet.svg";
+import "./Contacts.sass";
 const Contacts = ({ personalInfo }) => {
-  const { phone, email, location } = personalInfo;
+  const { phone, email, location, title, avatar } = personalInfo;
   return (
     <div className="contacts">
+        <img className="avatar" src={avatar} alt="Avatar" />
+      <div className="title-block">
+        <img className="title-icon" src={planetIcon} alt="planet Icon" />
+        <h2 className="title">{title}</h2>
+      </div>
       <div className="contacts-block">
         <img className="phone-icon" src={phoneIcon} alt="Phone Icon" />
         <p>{phone}</p>
@@ -14,7 +20,7 @@ const Contacts = ({ personalInfo }) => {
         <img className="phone-icon" src={letterIcon} alt="Letter Icon" />
         <p>{email}</p>
       </div>
-      <div className="contacts-block"> 
+      <div className="contacts-block">
         <img className="phone-icon" src={locationIcon} alt="Location Icon" />
         <p>{location}</p>
       </div>
