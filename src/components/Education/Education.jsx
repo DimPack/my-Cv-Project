2acf2629fe262d53d19e8c0aac12f96b44cfeae0
Dima <my_email@gmail.com>
2025-06-23 22:16:49 +0300

    create education block and experience

---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,23 @@
+import "./Education.sass";
+
+import "../../index.sass";
+const Education = ({ education }) => {
+  const { title, items } = education;
+  return (
+    <div>
+      <div className="title-block">
+        <h2 className="title">{title}</h2>
+      </div>
+        {items.map((edu, idx) => (
+            <div key={idx}>
+            <h3>{edu.institution}</h3>
+            <p>
+                {edu.degree} ({edu.startDate} - {edu.endDate})
+            </p>
+            </div>
+        ))}
+    </div>
+  );
+};
+
+export default Education;
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import './Experience.sass';
+import '../../index.sass';
+const Experience = ({ experiences }) => {
+  const { title, items } = experiences;
 
-const Experience = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="experience">
+      <div className="title-block">
+        <h2 className="title">{title}</h2>
+      </div>
+
+      {items.map((exp, idx) => (
+        <div key={idx}>
+          <h3>{exp.company}</h3>
+          <p>
+            {exp.position} ({exp.startDate} - {exp.endDate})
+          </p>
+          <ul className="experience-list">
+            {exp.description.map((desc, i) => (
+              <li key={i}>{desc}</li>
+            ))}
+          </ul>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default Experience;
